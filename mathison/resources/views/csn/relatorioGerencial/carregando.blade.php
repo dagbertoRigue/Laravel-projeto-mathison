c5dc8567b30a56688763b3f20625379a3a0f7cb7
Dagberto Rigue <riguealive@gmail.com>
2021-05-05 00:06:29 -0300

    Add files via upload

---
+++ mathison/resources/views/csn/relatorioGerencial/carregando.blade.php
@@ -0,0 +1,10 @@
+@extends('csn.templates.templateLoading')
+	@section('content')
+
+		<div class="loader">
+		  <div class="image">
+		    <i class="fa fa-line-chart" aria-hidden="true"></i>
+		  </div>
+		  <span></span>
+		</div>
+	@endsection
---
+++ mathison/resources/views/csn/relatorioGerencial/index.blade.php
@@ -0,0 +1,6 @@
+@extends('csn.templates.templateRelatorios')
+	@section('content')
+		@include('csn.relatorioGerencial.includes.navbarRelGer.navbarRelGer')
+		@include('csn.relatorioGerencial.includes.indexRelGer')
+		@include('csn.relatorioGerencial.includes.indexRelGerFooter')
+	@endsection
