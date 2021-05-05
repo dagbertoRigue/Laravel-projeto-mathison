7db5269987147d91843375e0dda3fb18b05831c3
Dagberto Rigue <riguealive@gmail.com>
2021-05-04 23:48:51 -0300

    Add files via upload

---
+++ mathison/resources/views/csn/home/index.blade.php
@@ -0,0 +1,10 @@
+@extends('csn.templates.templateHome')
+
+  @section('content')
+      @include('csn.includes.navbar')
+      @include('csn.includes.header2')
+      @include('csn.includes.tecnicas2')
+      @include('csn.includes.gerencial')
+      @include('csn.includes.contato')
+      @include('csn.includes.footer')
+  @endsection
