bb02e9e75db9d6d06562477489521ce572b196e6
Dagberto Rigue <riguealive@gmail.com>
2021-05-05 23:22:20 -0300

    Add files via upload

---
+++ mathison/resources/views/csn/relatorioGerencial/includes/indexRelGer.blade.php
@@ -0,0 +1,109 @@
+  <style>
+    .bg-footer {
+    background-color: #000;
+    padding: 22px;
+    }
+  </style>
+  <!-- Slider -->
+  <link href="/css/slider-dagb.css" rel="stylesheet">
+  <link href="/scss/slider-dagb.scss" rel="stylesheet">
+
+<div class="slider-container">
+  <div class="slider-control left inactive"></div>
+  <div class="slider-control right"></div>
+  <ul class="slider-pagi"></ul>
+
+  <div class="slider">
+    <div class="slide slide-0 active">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Bem vindo, ao Futuro da Manutenção</h2>
+          <p class="slide__text-desc">Manutenção Preditiva CSN-PR</p>
+          <!--<a class="slide__text-link">Project link</a>-->
+        </div>
+      </div>
+    </div>
+    <div class="slide slide-1 ">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Manutenção Preditiva</h2>
+          <p class="slide__text-desc">Acompanhamento periódico de equipamentos, através de dados coletados por meio de monitoração e inspeção.</p>
+          <!--<a class="slide__text-link">Project link</a>-->
+        </div>
+      </div>
+    </div>
+    <div class="slide slide-2">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Vida útil prolongada dos equipamentos</h2>
+          <p class="slide__text-desc">Predizendo o tempo de vida útil dos componentes e equipamentos.</p>
+          <!--<a class="slide__text-link">Project link</a>-->
+        </div>
+      </div>
+    </div>
+    <div class="slide slide-3">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Manutenção Preditiva</h2>
+          <p class="slide__text-desc">Redução de custos de manutenção e aumento da produtividade.</p>
+          <!--<a class="slide__text-link">Project link</a>-->
+        </div>
+      </div>
+    </div>
+    <div class="slide slide-4">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Análise de Vibração</h2>
+          <p class="slide__text-desc">Monitoração para diagnósticos de defeitos com riqueza de informação, extraída através da análise competente.</p>
+          <!--<a class="slide__text-link">Project link</a>-->
+        </div>
+      </div>
+    </div>
+    <div class="slide slide-5">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Análise Termográfica</h2>
+          <p class="slide__text-desc">Revolucionando a manutenção preditiva, através do espectro infravermelho da radiação.</p>
+          <!--<a class="slide__text-link">Project link</a>-->
+        </div>
+      </div>
+    </div>
+    <div class="slide slide-6">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">CSN Companhia Siderúrgica Nacional - PR</h2>
+          <p class="slide__text-desc">"Manutenção Competitiva para gerar valor"</p>
+          <!--<a class="slide__text-link">Project link</a>-->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
