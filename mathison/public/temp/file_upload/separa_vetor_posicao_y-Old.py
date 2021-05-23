58ccb5d00434a0b9cda485a3680ff95775160469
Dagberto Rigue <riguealive@gmail.com>
2021-05-22 22:52:10 -0300

    Add files via upload

---
+++ mathison/public/temp/file_upload/separa_vetor_posicao_y-Old.py
@@ -0,0 +1,14 @@
+#Script responsavel por ler os dados de um arquivo txt e separalos na posicao Y
+
+import numpy as np
+
+file1 = '/wamp/www/laravel/mathison/public/temp/file_upload/file_upload.txt'
+data1 = open(file1, 'rt')
+#vector = np.loadtxt(data1, delimiter=",")
+vector = data1.read().split(",")
+aux = 0
+y = 2
+
+for i in range(y):
+    aux = vector[i::y]
+print(aux)
